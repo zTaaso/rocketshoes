845409f172deb7049998022210f9bd6ab6668d6b
ztaaso <ztaaso.cedaspy@gmail.com>
2020-05-03 21:23:53 -0300

    configuring redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../utils/formater';
 
 import { ProductList } from './styles';
 
 import api from '../../services/api';
 
-export default function Home() {
+function Home() {
 	const [products, setProducts] = useState([]);
 
 	async function getProducts() {
 		const response = await api.get('/products');
 
-		setProducts(response.data);
+		const data = response.data.map((product) => ({
+			...product,
+			formatedPrice: formatPrice(product.price),
+		}));
+
+		setProducts(data);
 	}
 
 	useEffect(() => {
@@ -25,7 +32,7 @@ export default function Home() {
 					<img src={product.image} alt={product.title} />
 
 					<strong>{product.title}</strong>
-					<span>{product.price}</span>
+					<span>{product.formatedPrice}</span>
 					<button>
 						<div>
 							<MdAddShoppingCart size={20} color="#FFF" />2
@@ -38,3 +45,5 @@ export default function Home() {
 		</ProductList>
 	);
 }
+
+export default connect()(Home);
---
+++ src/utils/formater.js
@@ -0,0 +1,5 @@
+export const { format: formatPrice } = Intl.NumberFormat('pt-BR', {
+    currency: 'BRL',
+    style: 'currency'
+})
+
