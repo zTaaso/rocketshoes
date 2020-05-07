cc4c2597eb1160fa53d0b74494c88a012f069e9a
ztaaso <ztaaso.cedaspy@gmail.com>
2020-05-07 14:09:09 -0300

    check stock before add product

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,38 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../utils/formater';
 
 function* addToCart({ id }) {
-	const response = yield call(api.get, `/products/${id}`);
+	const product = yield select((state) => state.cart.find((p) => p.id === id));
 
-	yield put(addToCartSuccess(response.data));
+	const stock = yield call(api.get, `/stock/${id}`);
+
+	const stockAmount = stock.data.amount;
+	const currentAmount = product ? product.amount : 0;
+
+	const newAmount = currentAmount + 1;
+
+	if (newAmount > stockAmount) {
+		alert('Não temos mais estoque para esse produto!');
+		return;
+	}
+
+	if (product) {
+		yield put(updateAmount(id, newAmount));
+	} else {
+		const response = yield call(api.get, `/products/${id}`);
+
+		const data = {
+			...response.data,
+			amount: 1,
+			priceFormatted: formatPrice(response.data.price),
+		};
+
+		yield put(addToCartSuccess(data));
+	}
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
